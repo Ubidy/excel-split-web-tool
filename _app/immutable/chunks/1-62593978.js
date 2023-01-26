d1a0b52ff13b5916c5965b3b25f0101c58eb0453
nenjotsu <nenjotsu@protonmail.com>
2023-01-26 08:49:13 +0800

    Updates

--- _app/immutable/chunks/1-62593978.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-baa26497.js";export{t as component};
---
+++ _app/immutable/chunks/3-df3f4b76.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-523331c9.js";export{t as component};
